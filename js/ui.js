26ec8f315c17548eb170b3a778886754348e4949
Wyatt Preul <wpreul@gmail.com>
2013-04-29 12:30:28 -0500

    Now using client side TOC instead of jekyll plugin

---
+++ js/ui.js
@@ -0,0 +1,31 @@
+(function ($) {
+
+    $.toc = $.toc || {};
+
+    $.toc.register = function () {
+        var prevHeaderNum = 2;
+        var toc = $('<div><ul class="1"></ul></div>');
+        var level = 1;
+
+        $.each($('h2, h3, h4'), function(i, header) {
+            var headingText = $(header).text();
+            var headingId = headingText.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+            $(header).attr("id", headingId);
+            var headerNum = parseInt(header.tagName.replace(/H/i, ''));
+
+            if (prevHeaderNum === headerNum) {
+                $('ul.' + level + ':last', toc).append("<li><a href=\"#" + headingId + "\">" + headingText + "</a></li>");
+            }
+            else if (prevHeaderNum < headerNum) {
+                $('ul.' + level + ':last', toc).append("<li><ul class=\"" + ++level + "\"><li><a href=\"#" + headingId + "\">" + headingText + "</a></li></ul></li>");
+            }
+            else {
+                $('ul.' + --level + ':last', toc).append("<li><a href=\"#" + headingId + "\">" + headingText + "</a></li>");
+            }
+
+            prevHeaderNum = headerNum;
+        });
+
+        $('.sidebar').append(toc);
+    };
+})(jQuery);
